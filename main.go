b7b58d9db139e178033464c1719b6d4d117e758c
Paulo Cesar Vasco De Souza <paulo.vasco@mercadolivre.com>
2022-05-20 14:37:04 -0300

    Create main package

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bootcamp/food"
+	"fmt"
+)
+
+func main() {
+
+	var amount float64
+
+	animals := []struct {
+		name string
+		num  int
+	}{
+		{"dog", 10}, {"papagaio", 3}, {"hamster", 8}, {"tarantula", 3}, {"cat", 11}, {"cavalo", 2},
+	}
+
+	for _, animal := range animals {
+		quant, err := food.Animal(animal.name)
+		if err != nil {
+			fmt.Printf("%s: %s\n", animal.name, err.Error())
+			continue
+		}
+		amount += quant(animal.num)
+	}
+
+	fmt.Printf("Total: %.2f\n", amount)
+
+}
