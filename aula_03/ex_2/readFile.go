d8b69f29770ebc836b43fe8ff838eaecbb53f933
Paulo Cesar Vasco De Souza <paulo.vasco@mercadolivre.com>
2022-05-23 17:42:14 -0300

    Create function to read file from disk

---
+++ aula_03/ex_2/readFile.go
@@ -0,0 +1,42 @@
+package ex2
+
+import (
+	"bootcamp/aula_03/ex_2/model"
+	"encoding/csv"
+	"fmt"
+	"os"
+)
+
+func ReadFile() error {
+
+	// array to store results
+	var data []model.CSVFormat
+
+	file, err := os.Open("./saved_data.txt")
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	csvData, err := csv.NewReader(file).ReadAll()
+	if err != nil {
+		return err
+	}
+
+	for _, csvLine := range csvData {
+		dataLine := model.CSVFormat{
+			ID:       csvLine[0],
+			Price:    csvLine[1],
+			Quantity: csvLine[2],
+		}
+
+		data = append(data, dataLine)
+	}
+
+	for i, d := range data {
+		fmt.Println(fmt.Sprintf("%d. ID: %s, Price: %s, Quantity: %s", i, d.ID, d.Price, d.Quantity))
+	}
+
+	return nil
+}
