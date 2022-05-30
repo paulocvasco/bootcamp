e7e2597d2fb9d1fef767fd9ac0c1dc9feb258c8c
Paulo Cesar Vasco De Souza <paulo.vasco@mercadolivre.com>
2022-05-30 14:02:11 -0300

    Create pipeline to each sort algorithm

--- aula_04/ex_4/main.go
+++ aula_04/ex_4/main.go
@@ -1,56 +1,98 @@
 package main
 
 import (
+	"bootcamp/aula_04/ex_4/sort"
 	"bootcamp/aula_04/ex_4/types"
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 func main() {
-	variavel := rand.Perm(100)
-	fmt.Println(variavel)
-	count(variavel)
-	fmt.Println(variavel)
+	small := rand.Perm(100)
+	medium := rand.Perm(1000)
+	large := rand.Perm(10000)
+
+	fmt.Printf("Insertion: %+v\n", insertion(small, medium, large))
+	fmt.Printf("Selection: %+v\n", selection(small, medium, large))
+	fmt.Printf("Bubble: %+v\n", bubble(small, medium, large))
+
 }
 
-func insertion(nums types.Values) {
-	var i = 1
-	for i < nums.Len() {
-		var j = i
+func insertion(a, b, c types.Values) time.Duration {
+	first := make(chan struct{})
+	second := make(chan struct{})
+	third := make(chan struct{})
 
-		for j >= 1 && nums[j] < nums[j-1] {
-			nums[j], nums[j-1] = nums[j-1], nums[j]
-			j--
-		}
+	t1 := time.Now()
+	go func() {
+		sort.Insertion(a)
+		first <- struct{}{}
+	}()
+	go func() {
+		<-first
+		sort.Insertion(b)
+		second <- struct{}{}
+	}()
+	go func() {
+		<-second
+		sort.Insertion(c)
+		third <- struct{}{}
+	}()
+	<-third
+	t2 := time.Now()
 
-		i++
-	}
+	return t2.Sub(t1)
 }
 
-func selection(nums types.Values) {
-	for i := 0; i < nums.Len(); i++ {
-		var minIdx = i
-		for j := i; j < nums.Len(); j++ {
-			if nums.Less(j, minIdx) {
-				minIdx = j
-			}
-		}
-		nums.Swap(i, minIdx)
-	}
+func selection(a, b, c types.Values) time.Duration {
+	first := make(chan struct{})
+	second := make(chan struct{})
+	third := make(chan struct{})
+
+	t1 := time.Now()
+	go func() {
+		sort.Selection(a)
+		first <- struct{}{}
+	}()
+	go func() {
+		<-first
+		sort.Selection(b)
+		second <- struct{}{}
+	}()
+	go func() {
+		<-second
+		sort.Selection(c)
+		third <- struct{}{}
+	}()
+	<-third
+	t2 := time.Now()
+
+	return t2.Sub(t1)
 }
 
-func count(nums types.Values) {
-	var isDone = false
+func bubble(a, b, c types.Values) time.Duration {
+	first := make(chan struct{})
+	second := make(chan struct{})
+	third := make(chan struct{})
 
-	for !isDone {
-		isDone = true
-		var i = 0
-		for i < nums.Len()-1 {
-			if !nums.Less(i, i+1) {
-				nums.Swap(i, i+1)
-				isDone = false
-			}
-			i++
-		}
-	}
+	t1 := time.Now()
+	go func() {
+		sort.Bubble(a)
+		first <- struct{}{}
+	}()
+	go func() {
+		<-first
+		sort.Bubble(b)
+		second <- struct{}{}
+	}()
+	go func() {
+		<-second
+		sort.Bubble(c)
+		third <- struct{}{}
+	}()
+	<-third
+	t2 := time.Now()
+
+	return t2.Sub(t1)
 }
