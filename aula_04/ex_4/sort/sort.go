dc330c133c27a151f5ef39c5a536d13af74c9212
Paulo Cesar Vasco De Souza <paulo.vasco@mercadolivre.com>
2022-05-30 13:00:22 -0300

    Move functions to package

---
+++ aula_04/ex_4/sort/sort.go
@@ -0,0 +1,45 @@
+package sort
+
+import "bootcamp/aula_04/ex_4/types"
+
+func Insertion(nums types.Values) {
+	var i = 1
+	for i < nums.Len() {
+		var j = i
+
+		for j >= 1 && nums[j] < nums[j-1] {
+			nums[j], nums[j-1] = nums[j-1], nums[j]
+			j--
+		}
+
+		i++
+	}
+}
+
+func Selection(nums types.Values) {
+	for i := 0; i < nums.Len(); i++ {
+		var minIdx = i
+		for j := i; j < nums.Len(); j++ {
+			if nums.Less(j, minIdx) {
+				minIdx = j
+			}
+		}
+		nums.Swap(i, minIdx)
+	}
+}
+
+func Count(nums types.Values) {
+	var isDone = false
+
+	for !isDone {
+		isDone = true
+		var i = 0
+		for i < nums.Len()-1 {
+			if !nums.Less(i, i+1) {
+				nums.Swap(i, i+1)
+				isDone = false
+			}
+			i++
+		}
+	}
+}
