22c48d7b07b7d4d7bbd007e42df32c19a0bc880c
Paulo Cesar Vasco De Souza <paulo.vasco@mercadolivre.com>
2022-05-30 10:40:12 -0300

    Implement cli logic

--- aula_04/ex_1/main.go
+++ aula_04/ex_1/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bootcamp/aula_04/ex_1/types"
+	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -12,24 +14,60 @@ func main() {
 
 	var users types.Users
 
-	if len(os.Args) > 1 {
-		option = os.Args[1]
-		args = os.Args[2:]
-	}
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
 
-	switch option {
-	case "newuser":
-		if len(args) == 5 {
-			users.AddUser(args[0], args[1], args[2], args[3], args[4])
-		} else {
-			fmt.Println("Missing argument. Usage:")
-			fmt.Println("ex_1 newuser name lastname age email pass")
+		args = strings.Split(input.Text(), " ")
+		option = args[0]
+
+		switch option {
+		case "newuser":
+			if len(args) == 6 {
+				users.AddUser(args[1], args[2], args[3], args[4], args[5])
+			} else {
+				fmt.Println("Missing argument. Usage:")
+				fmt.Println("newuser name lastname age email pass")
+			}
+		case "changename":
+			if len(args) == 3 {
+				users.ChangeName(args[1], args[2])
+			} else {
+				fmt.Println("Missing argument. Usage:")
+				fmt.Println("changename username newname")
+			}
+		case "changeage":
+			if len(args) == 3 {
+				users.ChangeAge(args[1], args[2])
+			} else {
+				fmt.Println("Missing argument. Usage:")
+				fmt.Println("changeage username newage")
+			}
+		case "changelastname":
+			if len(args) == 3 {
+				users.ChangeLastName(args[1], args[2])
+			} else {
+				fmt.Println("Missing argument. Usage:")
+				fmt.Println("changelastname username newlastname")
+			}
+		case "changeemail":
+			if len(args) == 3 {
+				users.ChangeEmail(args[1], args[2])
+			} else {
+				fmt.Println("Missing argument. Usage:")
+				fmt.Println("changeemail username newemail")
+			}
+		case "changepass":
+			if len(args) == 3 {
+				users.ChangePassword(args[1], args[2])
+			} else {
+				fmt.Println("Missing argument. Usage:")
+				fmt.Println("changpass username newpass")
+			}
+		case "showusers":
+			users.ShowAllUsers()
+		default:
+			fmt.Println("Invalid option. Usage:")
+			fmt.Println("[newuser | changename | changeage | changelastname | changeemail | changepass] [params ...]")
 		}
-	default:
-		fmt.Println("Invalid option. Usage:")
-		fmt.Println("Save data: aula_3 -w [id] [price] [quantity]")
-		fmt.Println("Read data: aula_3 -r")
 	}
-
-	users.ShowAllUsers()
 }
