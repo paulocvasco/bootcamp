1ee7b56073502d5a56ac1d3b3f5689c020e108c5
Paulo Cesar Vasco De Souza <paulo.vasco@mercadolivre.com>
2022-06-02 12:41:30 -0300

    Create package to authentication

---
+++ aula_09/go-web/authentication/auth.go
@@ -0,0 +1,34 @@
+package authentication
+
+import (
+	customErrors "bootcamp/aula_09/go-web/custom_errors"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
+
+var token string
+
+func genToken() {
+	token = uuid.NewString()
+}
+
+func GetToken() string {
+	if token == "" {
+		genToken()
+	}
+	return token
+}
+
+func ValidateToken() gin.HandlerFunc {
+
+	return func(c *gin.Context) {
+		authToken := c.Request.Header.Get("token_authentication")
+		if authToken == "" || authToken != token {
+			c.AbortWithError(http.StatusUnauthorized, customErrors.ErrorInvalidToken)
+			return
+		}
+		c.Next()
+	}
+}
