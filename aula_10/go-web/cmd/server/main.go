12b9373a7aaf2a01817aa0a8b39c9c784e4b896c
Paulo Cesar Vasco De Souza <paulo.vasco@mercadolivre.com>
2022-06-02 23:51:46 -0300

    Clean routers and define service repos and controller

--- aula_10/go-web/cmd/server/main.go
+++ aula_10/go-web/cmd/server/main.go
@@ -1,23 +1,28 @@
 package main
 
 import (
-	"bootcamp/aula_10/go-web/authentication"
-	"bootcamp/aula_10/go-web/controller"
+	"bootcamp/aula_10/go-web/cmd/server/authentication"
+	"bootcamp/aula_10/go-web/cmd/server/controller"
+	"bootcamp/aula_10/go-web/internal/transactions/repository"
+	"bootcamp/aula_10/go-web/internal/transactions/services"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	repo := repository.NewRepository()
+	serv := services.NewService(repo)
+	controller := controller.NewController(serv)
+
 	r := gin.Default()
-	mapRouters(r)
+	mapRouters(r, controller)
 	r.Run()
 }
 
-func mapRouters(r *gin.Engine) {
-	r.GET("/hello", controller.Hello)
-	r.GET("/token", controller.GetToken)
-	r.GET("/transactions", authentication.ValidateToken(), controller.GetAll)
-	r.GET("/list/:field", authentication.ValidateToken(), controller.GetFieldValue)
-	r.GET("/info/:id", authentication.ValidateToken(), controller.GetObjectByID)
-	r.POST("/new", authentication.ValidateToken(), controller.AddObject)
+func mapRouters(r *gin.Engine, c controller.Controller) {
+	r.GET("/token", c.GetToken)
+	tr := r.Group("/transactions")
+	tr.POST("/", authentication.ValidateToken(), c.Store)
+	tr.GET("/all", authentication.ValidateToken(), c.GetAll)
+	tr.GET("/:id", authentication.ValidateToken(), c.GetByID)
 }
